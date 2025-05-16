209f9caf5266104f29f6d736d9f10cae836fe5d1
CHAMPION923 <1056156912@qq.com>
2025-05-16 13:46:47 +0800

    首次提交，初始化项目

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+namespace TestApp
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Button btn = new Button();
+            btn.Text = "Yes";
+            btn.Left = 10;
+            this.Controls.Add(btn);
+            btn.Top = 10;
+            btn.Click += OnButtonClick;
+            btn.MouseEnter += Btn_MouseEnter;
+        }
+
+        private void Btn_MouseEnter(object? sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void OnButtonClick(object? sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
+    }
+}
